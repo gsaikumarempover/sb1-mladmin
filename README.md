8373db24b1224610f031ca8aa4dc6f28c8698033
gsaikumarempover <153813874+gsaikumarempover@users.noreply.github.com>
2025-02-05 16:23:39 +0530

    remove unused files and configurations

--- README.md
+++
@@ -1,3 +0,0 @@
-# sb1-mladmin
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/gsaikumarempover/sb1-mladmin)
--- src/main/java/com/example/usermanagement/repository/UserTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.usermanagement.repository;
-
-import com.example.usermanagement.entity.UserType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTypeRepository extends JpaRepository<UserType, Long> {
-}
